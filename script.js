788eb2fdd111b3f7e5bd6c8333e30e8bd526e390
Blaser993 <corradodepinto.design@gmail.com>
2023-02-03 15:46:43 +0100

    FEAT  stampo i nomi delle mail in array con for

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ console.log("Mail");
 const mails = ["corrado_depinto@virgilio.it" , "corradodepinto.design@gmail.com", "blaser@hotmail.it" , "ibradado93@live.it" , "example@dominio.eu"];
 console.log(mails);
 
+let yourMail = prompt("Inserisci la tua mail");
+console.log(yourMail)
+
+for (let i = 0; i < mails.length; i++) {
+
+
+    let mailRegistered = i
+    console.log(mails[mailRegistered]);
+
+
+}
+
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che “cosa” ci serve?
